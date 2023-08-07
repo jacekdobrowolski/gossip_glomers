e3c28fd2875440c90f2500867fa60cad4b52ac44
Jacek Dobrowolski <jacek.ambrozy.dobrowolski@gmail.com>
2023-08-07 17:28:38 +0200

    singe node broadcast rpc layout

---
+++ maelstrom-broadcast/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"log"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+	messages := make([]int, 0, 200)
+
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		messages = append(messages, body["message"].(int))
+		body["type"] = "broadcast_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "read_ok"
+		body["messages"] = messages
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
